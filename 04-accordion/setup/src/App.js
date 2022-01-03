3b25dcbdda6637facaaff951297bcb98a446d4b1
zachuri <punsalang.zachary@gmail.com>
2022-01-03 15:14:30 -0800

    04: completed

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -2,7 +2,24 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+	const [questions, setQuestion] = useState(data);
+	console.log(questions);
+
+	return (
+		<main>
+			<div className="container">
+				<h3>Questions and Answers About Login</h3>
+				<section className="info">
+					{questions.map((question) => {
+						{
+							/* ...question is to pass all the props */
+						}
+						return <SingleQuestion key={question.id} {...question} />;
+					})}
+				</section>
+			</div>
+		</main>
+	);
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,25 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+
+const Question = ({ title, info }) => {
+	const [toggleBtn, setToggleBtn] = useState(false);
+
+	return (
+		<article className="question">
+			<header>
+				<h4>{title}</h4>
+				<button
+					className="btn"
+					onClick={() => {
+						setToggleBtn(!toggleBtn);
+					}}
+				>
+					{toggleBtn ? <AiOutlineMinus /> : <AiOutlinePlus />}
+				</button>
+			</header>
+			{toggleBtn && <p>{info}</p>}
+		</article>
+	);
 };
 
 export default Question;
