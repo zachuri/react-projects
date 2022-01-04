fd74ab4ca83c12ba1438ef10cb9d6c3614f2cff6
zachuri <punsalang.zachary@gmail.com>
2022-01-03 21:10:50 -0800

    05: able to dynamically show/add items when change in data

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -3,9 +3,22 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+// Using set to eliminate duplicates
+const allCatagories = ['all', ...new Set(items.map((item) => item.category))];
+// console.log(allCatagories);
+
 function App() {
 	const [menuItems, setMenuItems] = useState(items);
-	const [categories, setCategories] = useState([]);
+	const [categories, setCategories] = useState(allCatagories);
+
+	const filterItems = (category) => {
+		if (category === 'all') {
+			setMenuItems(items);
+			return;
+		}
+		const newItems = items.filter((item) => item.category === category);
+		setMenuItems(newItems);
+	};
 
 	return (
 		<section className="mian section">
@@ -13,7 +26,7 @@ function App() {
 				<h2>our menu</h2>
 				<div className="underline"></div>
 			</div>
-			<Categories />
+			<Categories categories={categories} filterItems={filterItems} />
 			<Menu items={menuItems} />
 		</section>
 	);
