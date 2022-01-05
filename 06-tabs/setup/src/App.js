e12001ba600fc7a7e7a83b5aa07090e606a7b959
zachuri <punsalang.zachary@gmail.com>
2022-01-05 14:21:42 -0800

    06: able to display all of the user data/ need to display others

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -7,7 +7,7 @@ const url = 'https://course-api.com/react-tabs-project';
 function App() {
 	const [loading, setLoading] = useState(true);
 	const [jobs, setJobs] = useState([]);
-	const [value, setValue] = useState(0);
+	const [value, setValue] = useState(1);
 
 	const fetchJobs = async () => {
 		const response = await fetch(url);
@@ -40,7 +40,20 @@ function App() {
 			<div className="jobs-center">
 				{/* btn container */}
 				{/*  job info */}
-				<article className="job-info"></article>
+            
+				<article className="job-info">
+					<h3>{title}</h3>
+					<h4>{company}</h4>
+					<p className="job-date">{dates}</p>
+					{duties.map((duty, index) => {
+						return (
+							<div key={index} className="job-desc">
+								<FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+								<p>{duty}</p>
+							</div>
+						);
+					})}
+				</article>
 			</div>
 		</section>
 	);
