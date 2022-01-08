1676d41aac581b9a5f481a04f9ef7f4cf1f2c071
zachuri <punsalang.zachary@gmail.com>
2022-01-07 20:38:57 -0800

    08: setup to display data

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,40 @@
 import React, { useState } from 'react';
 import data from './data';
+
 function App() {
-  
-  
-  return (
-  <h2>lorem ipsum project setup</h2>
-    )
+	const [count, setCount] = useState(0);
+	const [text, setText] = useState([]);
+
+	const handleSubmit = (e) => {
+		e.preventDefault(); // Uncessary page refressh
+		setText(data);
+	};
+
+	return (
+		<section className="section-center">
+			<h3>tired of boring lorem ipsum?</h3>
+			<form className="lorem-form" onSubmit={handleSubmit}>
+				<label htmlForm="amount">paragraphs:</label>
+				<input
+					type="number"
+					name="amount"
+					id="amount"
+					value={count}
+					onChange={(e) => {
+						setCount(e.target.value);
+					}}
+				></input>
+				<button type="submit" className="btn">
+					generate
+				</button>
+			</form>
+			<article className="lorem-text">
+				{text.map((item, index) => {
+					return <p key={index}>{item}</p>;
+				})}
+			</article>
+		</section>
+	);
 }
 
 export default App;
