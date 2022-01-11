13d7860d3252b3cf7d62c02b81db4b11e94edbad
zachuri <punsalang.zachary@gmail.com>
2022-01-10 18:23:02 -0800

    09: setup Copied Clipboard functionality

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -8,15 +8,21 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
 	const [alert, setAlert] = useState(false);
 	const bcg = rgb.join(',');
 	const hex = rgbToHex(...rgb);
-	console.log(bcg);
+	const hexValue = `#${hexColor}`;
 
+	console.log(bcg);
 	return (
 		<article
 			className={`color ${index > 10 && 'color-light'}`}
 			style={{ backgroundColor: `rgb(${bcg})` }}
+         onClick={() => {
+            setAlert(true); 
+            navigator.clipboard.writeText(hexValue)
+         }}
 		>
 			<p className="percent-value">{weight}%</p>
-			<p className="color-value">{hexColor}</p>
+			<p className="color-value">{hexValue}</p>
+			{alert && <p className="alert">copied to clipboard</p>}
 		</article>
 	);
 };
