abcdd357c5db180b88f10e747a67712f038debc2
zachuri <punsalang.zachary@gmail.com>
2022-01-20 22:10:13 -0800

    10: setup section with all components

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -1,9 +1,32 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from 'react';
+import List from './List';
+import Alert from './Alert';
 
 function App() {
-  return <h2>grocery bud setup</h2>
+	const [name, setName] = useState('');
+	const [list, setList] = useState([]);
+	const [isEditing, setIsEditing] = useState(false);
+	const [editId, setEditId] = useState(null);
+
+	// This is to pass in an object
+	const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('hello');
+	};
+
+	return (
+		<section className="section-center">
+			<form className="grocery-form" onSubmit={handleSubmit}>
+				{alert.show && <Alert />}
+			</form>
+
+			<div className="grocery-container">
+				<List />
+				<button className="clear-btn">clear items</button>
+			</div>
+		</section>
+	);
 }
 
-export default App
+export default App;
