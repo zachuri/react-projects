b66d54ea221f8ffbc760a1ca39b569c5e6d957f7
zachuri <punsalang.zachary@gmail.com>
2022-02-17 01:46:56 -0800

    10: completed adding items

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -11,7 +11,17 @@ function App() {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log('hello');
+
+		if (!name) {
+			// display alert
+		} else if (name && isEditing) {
+			// deal with edit
+		} else {
+			// show alert
+			const newItem = { id: new Date().getTime().toString(), title: name };
+			setList([...list, newItem]);
+			setName('');
+		}
 	};
 
 	return (
@@ -32,10 +42,14 @@ function App() {
 					</button>
 				</div>
 			</form>
-			<div className="grocery-container">
-				<List />
-				<button className="clear-btn">clear items</button>
-			</div>
+
+			{/* if list length is greater than 0 show clear div*/}
+			{list.length > 0 && (
+				<div className="grocery-container">
+					<List items={list} />
+					<button className="clear-btn">clear items</button>
+				</div>
+			)}
 		</section>
 	);
 }
