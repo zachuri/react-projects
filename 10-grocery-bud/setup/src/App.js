47cc49fef83705bdab54add442399951b5de9451
zachuri <punsalang.zachary@gmail.com>
2022-02-17 01:22:25 -0800

    10: restart/start

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -3,55 +3,7 @@ import List from './List';
 import Alert from './Alert';
 
 function App() {
-	const [name, setName] = useState('');
-	const [list, setList] = useState([]);
-	const [isEditing, setIsEditing] = useState(false);
-	const [editId, setEditId] = useState(null);
-
-	// This is to pass in an object
-	const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
-
-	const handleSubmit = (e) => {
-		e.preventDefault(); //prevent form from refreshing
-		console.log('hello');
-		if (!name) {
-			// display alert
-		} else if (name && isEditing) {
-			// deal with edit
-		} else {
-			// show alert
-			const newItem = { id: new Date().getTime().toString(), title: name };
-			setList([...list, newItem]);
-			setName('');
-		}
-	};
-
-	return (
-		<section className="section-center">
-			<form className="grocery-form" onSubmit={handleSubmit}>
-				{alert.show && <Alert />}
-				<h3>grocery bud</h3>
-				<div className="form-control">
-					<input
-						type="text"
-						className="grocery"
-						placeholder="e.g egggs"
-						value={name}
-						onChange={(e) => setName(e.target.value)}
-					/>
-					<button type="submit" className="submit-btn">
-						{isEditing ? 'edit' : 'submit'}
-					</button>
-				</div>
-			</form>
-			{list.length > 0 && (
-				<div className="grocery-container">
-					<List items={list} />
-					<button className="clear-btn">clear items</button>
-				</div>
-			)}
-		</section>
-	);
+	return <h2>gorcery bud setup</h2>;
 }
 
 export default App;
