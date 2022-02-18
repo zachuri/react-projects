9e92e652b729a1b9df91ad0020e647a7ef1d0d0e
zachuri <punsalang.zachary@gmail.com>
2022-02-18 00:39:01 -0800

    11: setup structure for navbar

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,10 +1,51 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from 'react';
+import { FaBars, FaTwitter } from 'react-icons/fa';
+import { links, social } from './data';
+import logo from './logo.svg';
 
 const Navbar = () => {
-  return <h4>navbar</h4>
-}
+	return (
+		<nav>
+			<div className="nav-center">
+				<div className="nav-header">
+					<img src={logo} alt="logo" className="" />
+					<button className="nav-toggle">
+						<FaBars />
+					</button>
+				</div>
+				<div className="links-container show-container">
+					<ul className="links">
+						<li>
+							<a href="#">home</a>
+						</li>
+						<li>
+							<a href="#">about</a>
+						</li>
+						<li>
+							<a href="#">contact</a>
+						</li>
+						<li>
+							<a href="#">products</a>
+						</li>
+					</ul>
+				</div>
+				<ul className="social-icons">
+                    <li>
+                        <a href="https://www.twitter.com"></a>
+                        <FaTwitter />
+                    </li>
+                    <li>
+                        <a href="https://www.twitter.com"></a>
+                        <FaTwitter />
+                    </li>
+                    <li>
+                        <a href="https://www.twitter.com"></a>
+                        <FaTwitter />
+                    </li>
+                </ul>
+			</div>
+		</nav>
+	);
+};
 
-export default Navbar
+export default Navbar;
