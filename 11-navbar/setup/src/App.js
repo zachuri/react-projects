6c766e2ab369022abdd5338e20999841d8e3a181
zachuri <punsalang.zachary@gmail.com>
2022-02-18 01:38:05 -0800

    11: removed sidebar (only used for example purposes)

--- 11-navbar/setup/src/App.js
+++ 11-navbar/setup/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from './Navbar';
-import Sidebar from './Sidebar';
+// import Sidebar from './Sidebar';
+
 function App() {
 	return (
 		<>
 			<Navbar />
-			<Sidebar />
+			{/* <Sidebar /> */}
 		</>
 	);
 }
--- 11-navbar/setup/src/Sidebar.js
+++ 11-navbar/setup/src/Sidebar.js
@@ -1,3 +1,4 @@
+// NO USE JUST FOR EXAMPLE PURPOSES
 import React from 'react';
 import { links, social } from './data';
 
